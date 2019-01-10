85b157a352c9e68c9a373bb7b6591375d132e2c3
Kitsch1 <kitsch94@gmail.com>
2019-01-10 17:39:31 +0900

    Problem No. 15649 and 15650 : N and M

---
+++ 15650.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+void solve(int start,int n,int m,bool selected[9],vector<int> &v){
+    int size = v.size();
+    if(size == m){
+        for(int i=0;i<size;i++){
+            printf("%d ",v[i]);
+        }
+        printf("\n");
+    }
+
+    for(int i=start;i<=n;i++){
+        if(selected[i] == false){
+            selected[i] = true;
+            v.push_back(i);
+            solve(i,n,m,selected,v);
+            v.pop_back();
+            selected[i] = false;
+        }
+    }
+}
+
+int main(){
+    int n,m;
+    bool selected[9];
+    scanf("%d %d",&n,&m);
+    memset(selected,false,sizeof(bool)*9);
+
+    for(int i=1;i<=n;i++){
+        vector<int> numbers;
+        selected[i] = true;
+        numbers.push_back(i);
+
+        solve(i,n,m,selected,numbers);
+
+        selected[i] = false;
+        numbers.pop_back();
+    }
+
+    return 0;
+}
