771e8cc1bae2f1c57b0f9e31655c5b0dbdb31cfc
Kitsch1 <kitsch94@gmail.com>
2019-03-06 16:04:14 +0900

    Problem No. 1016 (reference : Baekjoon's lecture)

--- 1016.cpp
+++ 1016.cpp
@@ -7,14 +7,14 @@ bool ans_check[1000001];
 int main(){
     lld l_min,l_max;
     cin >> l_min >> l_max;
-    lld l_cnt = l_max - l_min;
+    lld l_cnt = l_max - l_min; // calculate the range
     for(lld i=2;i*i<=l_max;i++){
         lld sq = i*i;
         lld start = sq - (l_min%sq); // make the start
         if(start == sq) start = 0; // need to check very first (if very first is nono number)
         for(lld j=start;j<=l_cnt;j+=sq){
             ans_check[j] = true;
-        }
+        } // check divided number
     }
     int ans = 0;
     for(int i=0;i<=l_cnt;i++){
