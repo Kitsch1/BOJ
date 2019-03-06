05762ea0e83387387a2c268f6586f070f0c60af1
Kitsch1 <kitsch94@gmail.com>
2019-03-06 10:40:32 +0900

    Problem No. 16234

---
+++ 16234.cpp
@@ -0,0 +1,75 @@
+#include <cstdio>
+#include <vector>
+#include <utility>
+#include <cstring>
+using namespace std;
+
+int n,lower,higher;
+int nations[50][50];
+bool visited[50][50];
+vector<pair<int,int>> changed;
+int popul = 0;
+
+const int r_way[4] = {0,0,1,-1};
+const int c_way[4] = {1,-1,0,0};
+
+int dfs(int r_cur,int c_cur){
+    int v_num = 1;
+    visited[r_cur][c_cur] = true;
+    changed.push_back({r_cur,c_cur});
+    popul += nations[r_cur][c_cur];
+    for(int i=0;i<4;i++){
+        int r_next = r_cur + r_way[i];
+        int c_next = c_cur + c_way[i];
+        if(r_next < 0 || r_next >= n || c_next < 0 || c_next >= n) continue;
+        int diff = nations[r_cur][c_cur] - nations[r_next][c_next];
+        if(diff < 0) diff = -diff;
+        if(!visited[r_next][c_next] && diff >= lower && diff <= higher){
+            v_num += dfs(r_next,c_next);
+        }
+    }
+    return v_num;
+}
+
+int solve(){
+    int ans = 0;
+    while(1){
+        bool move_flag = false;
+        for(int r=0;r<n;r++){
+            for(int c=0;c<n;c++){
+                if(visited[r][c]) continue;
+                int v_num = dfs(r,c);
+                if(v_num == 1){
+                    visited[r][c] = false;
+                }
+                else{
+                    if(!move_flag){
+                        move_flag = true; ans += 1;
+                    }
+                    int size = changed.size();
+                    int p_avg = popul / size;
+                    for(int i=0;i<size;i++){
+                        nations[changed[i].first][changed[i].second] = p_avg;
+                    }
+                }
+                changed.clear();
+                popul = 0;
+            }
+        }
+        if(!move_flag) break;
+        memset(visited,false,sizeof(visited));
+    }
+    return ans;
+}
+
+int main(){
+    scanf("%d %d %d",&n,&lower,&higher);
+    for(int r=0;r<n;r++){
+        for(int c=0;c<n;c++){
+            scanf("%d",&nations[r][c]);
+        }
+    }
+    printf("%d\n",solve());
+
+    return 0;
+}
